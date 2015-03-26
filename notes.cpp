f859e578f314b7b5efcde816ef7ccb7152b8e074
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2015-03-26 12:40:52 +0300

    add task4

---
+++ notes.cpp
@@ -0,0 +1,39 @@
+#include "stdafx.h"
+
+/*
+class graph
+{
+	void* _manager;
+
+	graph(void* manager) : _manager(manager) 
+	{
+	}
+};
+
+class Scene
+{
+	Matrix4d camera;
+
+	Scene(const Matrix4d& camera) 
+	{
+		this->camera = camera; 
+	}
+};
+
+
+
+
+class C
+{
+	C& Add(int x)
+	{
+		// ...
+		return *this;
+	}
+};
+
+C cnt;
+
+cnt.Add(1).Add(2).Add(3).Add(5); 
+
+*/
