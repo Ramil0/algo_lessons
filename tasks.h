dc33bddfcf8fdba055d985ea99bb3744eff4adbe
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2015-03-27 15:04:33 +0300

    add task 1.6

--- tasks.h
+++ tasks.h
@@ -1,13 +1,9 @@
 #include "stdafx.h"
 using namespace std;
 
-int task_1_1(const vector<int>& a);
-void task_1_2(int init_x, int init_y);
-void task_1_3(int M);
-int task_1_4(vector<int>& a);
-
 void test_task_1_1();
 void test_task_1_2();
 void test_task_1_3();
 void test_task_1_4();
 void test_task_1_5();
+void test_task_1_6();
