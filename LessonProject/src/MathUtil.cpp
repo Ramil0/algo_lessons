0bec67c2db51c996b97193e4a2e8fd60df4713d7
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2015-04-06 13:06:27 +0300

    .

---
+++ LessonProject/src/MathUtil.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+void print_vector(const vector<int>& a)
+{
+	for (int i = 0; i < a.size(); i++) 
+	{
+		cout << a[i] << " "; 
+	}
+	cout << endl;
+}
+
+void print_2d_vector(const vector<vector<int>>& a)
+{
+	cout << "<";
+	for (int i = 0; i < a.size(); i++) 
+	{
+		cout << "(";
+		for (int j = 0; j < a[i].size(); j++) 
+		{
+			cout << a[i][j] << " "; 
+		}
+		cout << "),";
+	}
+	cout  << ">" << endl;
+}
