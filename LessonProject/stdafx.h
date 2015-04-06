040d408b08cd7d22491b8d58d9cf8d3124c63f30
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2015-04-06 12:51:18 +0300

    change readme

--- LessonProject/stdafx.h
+++ LessonProject/stdafx.h
@@ -6,13 +6,16 @@
 #pragma once
 
 #include "targetver.h"
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <tchar.h>
 
-#include <Eigen/Dense>
 #include <string> 
 #include <iostream> 
 #include <vector>
 #include <list> 
 #include <map> 
+#include <Eigen/Dense>
+
+
+
